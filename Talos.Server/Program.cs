cecb8671102c7df1ca0a6586ce66d4ace83edbb2
Sergio Cortes <79613492+sergiocortes-dll@users.noreply.github.com>
2025-12-14 03:20:38 -0500

    remove ReferenceHandler.Preserve

--- Talos.Server/Program.cs
+++ Talos.Server/Program.cs
@@ -59,8 +59,6 @@ builder.Services.AddScoped<TemplateService>();
 builder.Services.AddControllers()
     .AddJsonOptions(options =>
     {
-        options.JsonSerializerOptions.ReferenceHandler =
-            System.Text.Json.Serialization.ReferenceHandler.Preserve;
         options.JsonSerializerOptions.WriteIndented = true;
     });
 
